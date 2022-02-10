5b3e23bf7774345b52cbdd401e3a9a332cb0bf38
Axel <axesode+github@gmail.com>
2022-02-10 23:05:45 +0100

    remove annoying default completion behaviour

--- src/.config/nvim/lua/plugins/configs/cmp.lua
+++ src/.config/nvim/lua/plugins/configs/cmp.lua
@@ -37,11 +37,10 @@ cmp.setup {
       ["<S-Tab>"] = cmp.mapping.select_prev_item(),
       ["<C-d>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.close(),
       ["<CR>"] = cmp.mapping.confirm {
          behavior = cmp.ConfirmBehavior.Insert,
-         select = true,
+         select = false,
       },
    },
    sources = {
